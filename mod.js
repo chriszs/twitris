93b86a800e073ed37d375c3ed361f47d2b18429a
Chris Zubak-Skees <chriszs@gmail.com>
2022-01-10 18:36:37 -0500

    Try to persist game state

--- mod.js
+++ mod.js
@@ -4,5 +4,12 @@ import Twitter from "./lib/twitter.js";
 const twitter = new Twitter();
 const game = new Game(8, 13);
 
+try {
+    await game.readFile('data/save.json');
+} catch (e) {
+    // no save
+}
 game.update();
+await game.writeFile('data/save.json');
+
 twitter.tweet(game.draw());
