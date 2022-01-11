ab92fe26fd7d2ae6ab3f28e5b8144fd81821ddf9
Chris Zubak-Skees <chriszs@gmail.com>
2022-01-10 22:34:52 -0500

    Accept moves from the crowd

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Game from "./lib/game.js";
-import twitter from "./lib/twitter.js";
+import Twitter from "./lib/twitter.js";
+
+const twitter = new Twitter();
 
 const game = new Game(8, 13);
 
@@ -11,12 +13,42 @@ try {
 game.update();
 await game.writeFile("data/save.json");
 
+const replies = await twitter.getReplies((await twitter.getLastTweet()).id);
+
+const left = replies.filter(
+  (tweet) =>
+    tweet.text.toLowerCase().includes("left") || tweet.text.includes("⬅️")
+).length;
+const right = replies.filter(
+  (tweet) =>
+    tweet.text.toLowerCase().includes("right") || tweet.text.includes("➡️")
+).length;
+const spin = replies.filter(
+  (tweet) =>
+    tweet.text.toLowerCase().includes("spin") ||
+    tweet.text.toLowerCase().includes("tilt") ||
+    tweet.text.toLowerCase().includes("turn") ||
+    tweet.text.toLowerCase().includes("flip") ||
+    tweet.text.includes("⤴️")
+).length;
+
+if (spin > 0 && spin > left && spin > right) {
+  console.log("spin right");
+  game.tiltRight();
+} else if (left > 0 && left > right) {
+  console.log("go left");
+  game.left();
+} else if (right > 0) {
+  console.log("go right");
+  game.right();
+}
+
 const status = game.draw();
 
 console.log(status);
 
 try {
-  await twitter.post("statuses/update", { status });
+  // await twitter.tweet(status);
 } catch (e) {
   console.log(e);
 }
